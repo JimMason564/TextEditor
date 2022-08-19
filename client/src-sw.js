019524bcb0cfd99963603c83af3a3cfcc9223494
Jim Mason <jimmason564@yahoo.com>
2022-08-18 21:52:46 -0700

    Correcting index.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
-const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
+const { warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
@@ -27,8 +27,8 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
-({ request }) => ["style", "script", "worker"].includes(request.destination),
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
     cacheName: "asset-cache",
     plugins: [
@@ -36,4 +36,5 @@ registerRoute();
         statuses: [0, 200],
       }),
     ],
-  });
+  })
+);
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -24,13 +24,10 @@ if (typeof editor === 'undefined') {
 }
 
 // Check if service workers are supported
-registerRoute();
-({ request }) => ["style", "script", "worker"].includes(request.destination),
-  new StaleWhileRevalidate({
-    cacheName: "asset-cache",
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  });
+if ('serviceWorker' in navigator) {
+  // register workbox service worker
+  const workboxSW = new Workbox('/src-sw.js');
+  workboxSW.register();
+} else {
+  console.error('Service workers are not supported in this browser.');
+}
